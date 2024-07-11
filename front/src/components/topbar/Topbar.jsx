60ca155e40d91f5ce76d8b939be5e673a8ff933d
BySamm <sam.mba.byiringiro@gmail.com>
2024-07-11 10:18:39 +0200

    Adding logout icon on topBar

--- front/src/components/topbar/Topbar.jsx
+++ front/src/components/topbar/Topbar.jsx
@@ -25,7 +25,7 @@ export default function Topbar() {
         <div className="searchbar">
           <Search className="searchIcon" />
           <input
-            placeholder="Search for friend, post or video"
+            placeholder="Search for artist, post or video"
             className="searchInput"
           />
         </div>
